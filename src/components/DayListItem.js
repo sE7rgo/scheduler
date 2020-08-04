b4b2fbe7e903f46e6e36198d9758f12505b698c0
sE7rgo <se7rgo@gmail.com>
2020-08-04 14:36:55 -0700

    instead of rendering num of spots, renders string with how many spots left

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,12 +4,19 @@ import "components/DayListItem.scss"
 
 export default function DayListItem(props) {
 
-  console.log(props)
   let dayClass = className("day-list__item", 
     {
       "day-list__item--selected": props.selected,
       "day-list__item--full": props.spots === 0
     })
+
+
+  const formatSpots = (props) =>
+    props.spots === 0
+      ? 'no spots remaining'
+      : props.spots === 1
+      ? `${props.spots} spot remaining`
+      : `${props.spots} spots remaining`;
 
   return (
     <li
@@ -17,7 +24,7 @@ export default function DayListItem(props) {
       className={dayClass}
     >
       <h2>{props.name}</h2> 
-      <h3>{props.spots}</h3>
+      <h3>{formatSpots(props)}</h3>
     </li>
   );
 }
