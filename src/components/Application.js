fd47338223eed9564a86b317391c986fb6a9a55b
sE7rgo <se7rgo@gmail.com>
2020-08-06 12:29:45 -0700

    fetched days api with with days hook

--- src/components/Application.js
+++ src/components/Application.js
@@ -60,36 +60,25 @@ const appointments = [
 
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
 
 export default function Application(props) {
-
-  const [day, setDay] = useState("Monday");
-
+  
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    appointments: {}
+  });
+  
+  const setDay = day => setState({ ...state, day });
+  const setDays = days => setState(prev => ({...prev, days}))
 
   useEffect(() => {
-    axios.get("/api/day")
+    axios.get('/api/days')
     .then((response) => {
-      setDay(response)
-      console.log(response)
+      setDays(response.data)
+      console.log(response.data)
     })
-  }, [])
+  },[])
 
   return (
     <main className="layout">
@@ -102,8 +91,8 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
           <DayList
-            days={days}
-            day={day}
+            days={state.days}
+            day={state.day}
             setDay={setDay}
           />
         </nav>
